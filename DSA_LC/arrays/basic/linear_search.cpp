cd9935aad14d839885f958e065d05856241f01fd
Om Thakare <124353487+thakare-om03@users.noreply.github.com>
2025-01-27 15:33:22 +0530

    Add linear search

---
+++ DSA_LC/arrays/basic/linear_search.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    bool linear_search(vector<int>& arr, int target)
+    {
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i]==target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
