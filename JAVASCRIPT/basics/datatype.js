d1bd03157efa20b484c577a50034438ea71b2f90
Om Thakare <124353487+thakare-om03@users.noreply.github.com>
2025-02-20 10:57:46 +0530

    Learnt basic datatypes in Js

---
+++ JAVASCRIPT/basics/datatype.js
@@ -0,0 +1,15 @@
+// Datatypes in Javascript
+let score = 100; // Number
+let name = "Om"; // String
+console.log(name);
+let isTrue = true; // Boolean
+let isFalse = false; // Boolean
+let a; // Undefined
+let b = null; // Null
+let users = { firstname: "om", lastname: "thakare" }; // Object
+let teaTypes = ["Green", "Black", "White"]; // Array
+let e = 10;
+let f = 20;
+let c = e + f;
+console.log(c);
+let getScore = score;
