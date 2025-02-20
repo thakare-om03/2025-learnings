c841569751d79dd21ca791c9854af42824ec5b45
Om Thakare <124353487+thakare-om03@users.noreply.github.com>
2025-02-20 11:19:14 +0530

    Learnt basic operations in Js

---
+++ JAVASCRIPT/basics/operations.js
@@ -0,0 +1,49 @@
+// Perform addition
+let addition = 10 + 50;
+console.log(addition);
+
+// Define base and bonus
+let base = 11000;
+let bonus = 5000;
+
+// Calculate total salary
+let totalSalary = base + bonus;
+console.log(totalSalary);
+
+// Division
+let div = 8 / 2;
+console.log(div);
+
+// Exponentiation
+let expo = 2 ** 3;
+console.log(expo);
+
+// Modulo operation
+let mod = 10 % 3;
+console.log(mod);
+
+// Subtraction
+let sub = 10 - 3;
+console.log(sub);
+
+// Multiplication
+let mul = 10 * 3;
+console.log(mul);
+
+// Increment & decrement
+let inc = 10;
+inc++;
+let credits = 100;
+credits--;
+console.log(inc);
+console.log(credits);
+
+// Comparisons
+let num1 = 10;
+let num2 = 20;
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
